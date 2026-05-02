528c353b946168c4a6c9e0f771af3cb82ae2b318
ddebien <sir.dave.debien@gmail.com>
2026-05-02 09:38:07 +0200

    Laboratory Experiment 1 by ddebien

---
+++ gate-level-modelling/ddebien/and_gate/and_gate.v
@@ -0,0 +1,18 @@
+//-----------------------------------------------------
+// Laboratory Experiment 001
+// Design Name : and_gate
+// File Name : and_gate.v
+// Function : Implement AND logic gate
+// Designer: Dave DeBien
+// Period: Term 3 AY25-26
+//-----------------------------------------------------
+
+module and_gate(
+  input A,B,
+  output C
+  );
+  
+  // Gate type
+  and  ddebien (C, A, B);
+
+endmodule
