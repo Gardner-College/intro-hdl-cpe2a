12956e5a4206c585d5e7d8abfd36c3f20f000e10
AEdoria <edoria.ajkean@gmail.com>
2026-05-11 02:43:49 +0800

    Laboratory Experiment 1 by aedoria

---
+++ gate-level-modelling/aedoria/nand_gate/nand_gate.v
@@ -0,0 +1,18 @@
+//---------------------------------
+// Laboratory Experiment 001
+// Design Name : nand_gate
+// File Name :nand_gate.v
+// Function : Implement AND logic gate
+// Designer : Aj Kean Edoria
+// Period : Term 3 AY25-26
+//---------------------------------
+
+module nand_gate(
+	input A,B,
+	output C
+	);
+	
+	//Gate type
+	nand aedoria (C, A, B);
+	
+	endmodule
---
+++ gate-level-modelling/aedoria/xnor_gate/xnor_gate.v
@@ -0,0 +1,18 @@
+//---------------------------------
+// Laboratory Experiment 001
+// Design Name : xnor_gate
+// File Name :xnor_gate.v
+// Function : Implement AND logic gate
+// Designer : Aj Kean Edoria
+// Period : Term 3 AY25-26
+//---------------------------------
+
+module xnor_gate(
+	input A,B,
+	output C
+	);
+	
+	//Gate type
+	xnor aedoria (C, A, B);
+	
+	endmodule
