2992a44dd144e695df7f48737e6aadeec3682fab
zuget <zugetsu21@gmail.com>
2026-06-27 17:01:59 +0800

    laboratory experiment

---
+++ gate-level-modelling/geudela/not_gate--geloeudela/not_gate.v
@@ -0,0 +1,18 @@
+//-----------------------------------------------------
+// Laboratory Experiment 001
+// Design Name : not_gate
+// File Name : not_gate.v
+// Function : Implement AND logic gate
+// Designer: Marjune Angelo Eudela
+// Period: Term 3 AY25-26
+//-----------------------------------------------------
+
+module not_gate(
+  input A,
+  output C
+  );
+  
+  // Gate type
+  not Gelo	 (C, A);
+
+endmodule
