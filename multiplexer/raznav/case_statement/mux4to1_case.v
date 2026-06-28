6360279b77a1c1ae715dbb94bced320f3496653a
raz-el650 <navalesrazel828@gmail.com>
2026-06-29 01:28:57 +0800

    Laboratory Expirement 007

---
+++ multiplexer/raznav/case_statement/mux4to1_case.v
@@ -0,0 +1,23 @@
+//-----------------------------------------------------
+// Design Name : case statement
+// File Name   : case statement
+// Function    : 4:1 Multiplexer
+// Coder       : Razel Navales
+//-----------------------------------------------------
+module mux4to1_case(
+    input c0, c1, c2, c3,
+    input A, B,
+    output reg Y
+);
+
+always @(*) begin
+    case ({B, A})
+        2'b00: Y = c0;
+        2'b01: Y = c1;
+        2'b10: Y = c2;
+        2'b11: Y = c3;
+        default: Y = 1'b0;
+    endcase
+end
+
+endmodule
---
+++ multiplexer/raznav/if_statement/mux4to1_if.v
@@ -0,0 +1,24 @@
+//-----------------------------------------------------
+// Design Name : if_statement
+// File Name   : if_statement.v
+// Function    : 4:1 Multiplexer
+// Coder       : Razel Navales
+//-----------------------------------------------------
+module mux4to1_if(
+    input c0, c1, c2, c3,
+    input A, B,
+    output reg Y
+);
+
+always @(*) begin
+    if (B == 0 && A == 0)
+        Y = c0;
+    else if (B == 0 && A == 1)
+        Y = c1;
+    else if (B == 1 && A == 0)
+        Y = c2;
+    else
+        Y = c3;
+end
+
+endmodule
