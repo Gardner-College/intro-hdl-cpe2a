195e52fe50eca1af5b4d5bf81b308c98578fcf86
Jasid <jheboybilbao@gmail.com>
2026-06-29 03:06:09 +0800

    Laboratory Experiment 007 : 4-to-1 Multiplexer

---
+++ multiplexer/jheboy/logic_gates/mux4to1_logic.v
@@ -0,0 +1,29 @@
+//-----------------------------------------------------
+// Design Name : logic gates
+// File Name   : logic gates
+// Function    : 4:1 Multiplexer
+// Coder       : Jheboy B. Asid
+//-----------------------------------------------------
+module mux4to1_logic(
+    input c0, c1, c2, c3,
+    input A, B,
+    output Y
+);
+
+wire nA, nB;
+wire w0, w1, w2, w3;
+
+// NOT Gates
+not (nA, A);
+not (nB, B);
+
+// AND Gates
+and (w0, c0, nA, nB);
+and (w1, c1, A,  nB);
+and (w2, c2, nA, B);
+and (w3, c3, A,  B);
+
+// OR Gate
+or (Y, w0, w1, w2, w3);
+
+endmodule
